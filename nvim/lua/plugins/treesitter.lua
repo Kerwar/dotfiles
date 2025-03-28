3def77145ffe62de1d414b1acdfce786ae04bfa7
Kerwar <j.bosch94.bc@gmail.com>
2025-03-28 13:39:48 +0100

    Added basic treesitter setup.

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.configs", -- Sets main module to use for opts
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"cpp",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"vim",
+			"vimdoc",
+		},
+		highlight = {
+			enable = true,
+		},
+		indent = { enable = true },
+	},
+}
