174e423e95ac77eb8a4911355ef9d18c9316b4a6
Kerwar <j.bosch94.bc@gmail.com>
2025-04-08 10:27:09 +0200

    Added mini and modify lspconfig and treesitter to have better keymaps.

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,38 +1,59 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
+    -- Automatically install LSPs and related tools to stdpath for Neovim
+    {
+      "williamboman/mason.nvim",
+      config = true,
+    },
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
     {
       "folke/lazydev.nvim",
       ft = "lua",
       opts = {
         library = {
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } }
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
       },
     },
+    -- Useful status updates for LSP.
+    { "j-hui/fidget.nvim", opts = {} },
   },
+
   config = function()
     require("lspconfig").lua_ls.setup({})
     require("lspconfig").clangd.setup({})
 
-    vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format() end)
+    vim.keymap.set("n", "<space>f", function()
+      vim.lsp.buf.format()
+    end)
 
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('my.lsp', {}),
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("my.lsp", {}),
       callback = function(args)
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
         -- Auto-format ("lint") on save.
         -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-        if not client:supports_method('textDocument/willSaveWaitUntil')
-            and client:supports_method('textDocument/formatting') then
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+        if
+          not client:supports_method("textDocument/willSaveWaitUntil")
+          and client:supports_method("textDocument/formatting")
+        then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
             buffer = args.buf,
             callback = function()
               vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
             end,
           })
+        end
+
+        -- Creates a keymap to toggle inlay hints in your
+        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+          vim.map("<leader>th", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = args.buf }))
+          end, "[T]oggle Inlay [H]ints")
         end
       end,
     })
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,25 @@
+-- Highlight, edit, and navigate code
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs", -- Sets main module to use for opts
-	opts = {
-		ensure_installed = {
-			"bash",
-			"c",
-			"cpp",
-			"diff",
-			"html",
-			"lua",
-			"luadoc",
-			"markdown",
-			"markdown_inline",
-			"vim",
-			"vimdoc",
-		},
-		highlight = {
-			enable = true,
-		},
-		indent = { enable = true },
-	},
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs", -- Sets main module to use for opts
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "cpp",
+      "diff",
+      "html",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "vim",
+      "vimdoc",
+    },
+    highlight = {
+      enable = true,
+    },
+    indent = { enable = true },
+  },
 }
