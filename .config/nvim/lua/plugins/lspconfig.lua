833b948849964b5f954b626fb0c5bbb5a4556d66
Kerwar <j.bosch94.bc@gmail.com>
2025-07-15 14:55:55 +0200

    Removed mason and simplified lsp

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,13 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    -- Automatically install LSPs and related tools to stdpath for Neovim
-    {
-      "williamboman/mason.nvim",
-      config = true,
-    },
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
     {
       "folke/lazydev.nvim",
       ft = "lua",
@@ -25,23 +18,24 @@ return {
     local servers = {
       lua_ls = {},
       bash_ls = {},
-      clangd = {
-        filetypes = { 'c', 'cpp', 'cc' },
-      },
       cmake = {
       },
       rust_analyzer = {},
       slint_lsp = {},
       zls = {}
     }
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-    -- 0.11 Form of enable lsp probably hace to change in the future.
-    -- vim.lsp.enable({ 'lua_ls', 'clangd', 'cmake' })
-    -- vim.lsp.enable('zls')
-    --
-    -- vim.lsp.config('clangd', {
-    --   filetypes = { 'c', 'cpp', 'cc' },
-    -- })
+    vim.lsp.config("*", { capabilities = capabilities })
+    vim.lsp.enable({
+      "bash_ls",
+      "clangd",
+      "cmake",
+      "lua_ls",
+      "rust_analyzer",
+      "slint_lsp",
+      "zls",
+    })
 
     vim.keymap.set("n", "<space>f", function()
       vim.lsp.buf.format()
@@ -72,23 +66,6 @@ return {
           })
         end
 
-        local map = function(keys, func, desc, mode)
-          mode = mode or 'n'
-          vim.keymap.set(mode, keys, func, { buffer = args.buf, desc = 'LSP: ' .. desc })
-        end
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, { buffer = args.buf })
-        -- Rename the variable under your cursor.
-        --  Most Language Servers support renaming across files, etc.
-        map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
-        map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
-
-        -- WARN: This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header.
-        map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
         -- Creates a keymap to toggle inlay hints in your
         if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
           vim.keymap.set("n", "<leader>th", function()
@@ -97,24 +74,5 @@ return {
         end
       end,
     })
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-    require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
-      automatic_enable = true,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          vim.lsp.enable(server)
-          vim.lsp.config(server)
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
   end,
 }
